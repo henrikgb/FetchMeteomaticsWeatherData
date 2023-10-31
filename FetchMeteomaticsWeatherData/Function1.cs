7d10eb02051c9da93a27981ebd2cf947d2175137
HBU <henrik-gb@hotmail.com>
2023-10-31 13:33:03 +0100

    Rename variable to remove non-ASCII character
    Changed the variable name from `varname_with_ø` to `varname_with_o` to
    ensure better compatibility with tools and environments that may not
    handle non-ASCII characters well.

--- FetchMeteomaticsWeatherData/Function1.cs
+++ FetchMeteomaticsWeatherData/Function1.cs
@@ -146,7 +146,7 @@ namespace WeatherFunctionApp
         {
             new Coordinate { Latitude = 59.02050003940309, Longitude = 5.592325942611728, NameId = "Sande" },
             new Coordinate { Latitude = 58.88531361351894, Longitude = 5.602662428854268, NameId = "Sola" },
-            new Coordinate { Latitude = 58.84227538997773, Longitude = 5.560516132410946, NameId = "Hellestø" },
+            new Coordinate { Latitude = 58.84227538997773, Longitude = 5.560516132410946, NameId = "Hellesto" },
             new Coordinate { Latitude = 58.81231852222533, Longitude = 5.546945324943648, NameId = "Sele" },
             new Coordinate { Latitude = 58.80123195118268, Longitude = 5.5480941336096645, NameId = "Bore" },
             new Coordinate { Latitude = 58.740441600947264, Longitude = 5.512925570900187, NameId = "Orre" },
